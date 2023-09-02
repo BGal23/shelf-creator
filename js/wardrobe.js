01a4fd68256872864a53e2cd985e0924ea8df953
BGal23 <131159103+BGal23@users.noreply.github.com>
2023-09-02 20:14:01 +0200

    ggr

--- js/wardrobe.js
+++ js/wardrobe.js
@@ -1,11 +1,17 @@
 const buttonWardrobe = document.getElementById("wardrobe-generate")
+const type = document.querySelector(".wardrobe__title")
 let bok = {};
-// let kranc = {};
-// let pułka = {};
-// let cokół = {};
-// let plecy = {};
+let kranc = {};
+let cokół = {};
+let półka = {};
+let plecy = {};
+let front = {};
+let szafa = [];
+
 
 const wardrobe = () => {
+    let name = document.getElementById("wardrobe-name").value;
+    let quantity = document.getElementById("wardrobe-quantity").value;
     let height = document.getElementById("wardrobe-height").value;
     let width = document.getElementById("wardrobe-width").value;
     let deep = document.getElementById("wardrobe-deep").value;
@@ -13,18 +19,73 @@ const wardrobe = () => {
     let plinth = document.getElementById("wardrobe-plinth").value;
     let shelfs = document.getElementById("wardrobe-shelfs").value;
 
-    bok.ilość = 2;
-    bok.długość = {
-        wymiar: height,
-        okleina: 2
-    };
-    bok.szerokość = {
-        wymiar: width,
-        okleina: 0
-    };
-    bok.grubość = plate;
+    bok = {
+        nazwa: name,
+        rodzaj: `${type.textContent}-bok`,
+        ilość: 2 * quantity,
+        długość: height,
+        okleinaDł: 2,
+        szerokość: deep,
+        okleinaSzer: 2,
+        grubość: plate
+    }
 
-    console.log(bok)
+    kranc = {
+        nazwa: name,
+        rodzaj: `${type.textContent}-kranc`,
+        ilość: 2 * quantity,
+        długość: width - 2 * plate,
+        okleinaDł: 2,
+        szerokość: deep,
+        okleinaSzer: 0,
+        grubość: plate
+    }
+
+    cokół = {
+        nazwa: name,
+        rodzaj: `${type.textContent}-cokół`,
+        ilość: quantity,
+        długość: width - 2 * plate,
+        okleinaDł: 1,
+        szerokość: plinth,
+        okleinaSzer: 0,
+        grubość: plate
+    }
+
+    półka = {
+        nazwa: name,
+        rodzaj: `${type.textContent}-półka`,
+        ilość: 2 * quantity,
+        długość:  width - 2 * plate,
+        okleinaDł: 2,
+        szerokość: deep - 10,
+        okleinaSzer: 0,
+        grubość: plate
+    }
+
+    plecy = {
+        nazwa: name,
+        rodzaj: `${type.textContent}-plecy`,
+        ilość: quantity,
+        długość: height - plinth - 6,
+        okleinaDł: 0,
+        szerokość: width - 6,
+        okleinaSzer: 0,
+        grubość: plate
+    }
+
+    front = {
+        nazwa: name,
+        rodzaj: `${type.textContent}-front`,
+        ilość: quantity,
+        długość: height,
+        okleinaDł: 2,
+        szerokość: width,
+        okleinaSzer: 2,
+        grubość: plate
+    }
+
+    console.log(bok, kranc, cokół, półka, plecy, front)
 }
 
 buttonWardrobe.addEventListener("click", wardrobe)
